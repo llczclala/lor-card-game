1d1c51c172705f60f3e3f2d3af7dea46fbab2c8d
llczclala <luoliangcheng502@gmail.com>
2026-01-08 22:31:18 +0800

    snowbreakrivals

--- src/components/GameUI.tsx
+++ src/components/GameUI.tsx
@@ -81,7 +81,7 @@ export const NexusDisplay = ({
 };
 
 // [修改] 牌库组件：接收 cardBackIndex
-export const Deck = ({ isEnemy, cardBackIndex = 0 }: { isEnemy: boolean, cardBackIndex?: number }) => {
+export const Deck = ({ _isEnemy, cardBackIndex = 0 }: { _isEnemy?: boolean, cardBackIndex?: number }) => {
     // 获取对应的卡背图片，如果索引无效则回退到默认(0)
     const cardBackImg = PERSONALIZATION_ASSETS.cardBacks[cardBackIndex] || PERSONALIZATION_ASSETS.cardBacks[0];
 
